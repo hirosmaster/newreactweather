3e4849a68c5597038102997fc5edd94f777cc4b5
Andrea <andrea.soriano@outlook.com>
2024-08-26 15:43:50 +0800

    started weather js file
    made placeholder weather box with some css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import "./App.css";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Weather</h1>
+      <div className="container">
+        <h1>Weather</h1>
+        <Weather />
+
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/hirosmaster" target="_blank">
+            Rica Andrea Soriano
+          </a>
+          and is open sourced on{" "}
+          <a
+            href="https://github.com/hirosmaster/newreactweather"
+            target="_blank"
+          >
+            Github
+          </a>
+          and is hosted on{" "}
+          <a
+            href="https://master--boopyboopweather.netlify.app/"
+            target="_blank"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
