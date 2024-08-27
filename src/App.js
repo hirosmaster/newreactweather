8a86ca68de46cab002d0af0d4ad1b55b51b2cfef
Andrea <andrea.soriano@outlook.com>
2024-08-27 15:24:42 +0800

    played around with css
    updated css

--- src/App.js
+++ src/App.js
@@ -4,31 +4,24 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
-        <h1>Weather</h1>
-        <Weather />
-
-        <footer>
-          This project was coded by{" "}
-          <a href="https://github.com/hirosmaster" target="_blank">
-            Rica Andrea Soriano
-          </a>
-          and is open sourced on{" "}
-          <a
-            href="https://github.com/hirosmaster/newreactweather"
-            target="_blank"
-          >
-            Github
-          </a>
-          and is hosted on{" "}
-          <a
-            href="https://master--boopyboopweather.netlify.app/"
-            target="_blank"
-          >
-            Netlify
-          </a>
-        </footer>
-      </div>
+      <Weather />
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/hirosmaster" target="_blank">
+          Rica Andrea Soriano{" "}
+        </a>
+        and is open sourced on{" "}
+        <a
+          href="https://github.com/hirosmaster/newreactweather"
+          target="_blank"
+        >
+          Github
+        </a>
+        and is hosted on{" "}
+        <a href="https://master--boopyboopweather.netlify.app/" target="_blank">
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -5,30 +5,38 @@ export default function Weather() {
     <div className="Weather">
       <form>
         <div className="row">
-          <div className="col-9">
+          <div className="col-4">
             <input
               type="search"
               placeholder="Enter a city"
               className="search-form"
+              autoFocus="on"
             />
           </div>
-          <div className="col-3">
+          <div className="col-8">
             <input type="submit" value="Search" className="button" />
           </div>
         </div>
       </form>
-      <h1>Rome</h1>
-      <ul>
-        <li>Monday 15:30</li>
-        <li>Mostly cloudy</li>
-      </ul>
-      <div className="row">
+      <div className="row" id="header">
+        <div className="col-6">
+          <h1>Rome</h1>
+        </div>
         <div className="col-6">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
             alt="very sunny"
           />
-          36°C
+          <span className="temp">36</span>
+          <span className="tempUnit">°C</span>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <ul>
+            <li>Monday 15:30</li>
+            <li>Mostly cloudy</li>
+          </ul>
         </div>
         <div className="col-6">
           <ul>
